d333589bc2f86d718f358aca2c6f6ab7c722196c
kiko2183 <romankulicnij@gmail.com>
2024-08-18 18:23:31 +0300

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; 
-import { authReducer } from './auth/authSlice';
+import { authReducer } from './auth/slice';
 import { contactsReducer } from './contacts/contactsSlice';
 import { filtersReducer } from './filters/filtersSlice';
 
